e12a7061cbe99eeeb184ad3d50b881f1577d4263
zoliantsoa62-cell <zoliantsoa62@gmail.com>
2026-06-18 15:09:45 +0300

    Remove unnecessary comment about Render

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-# Création des tables dès le démarrage (important pour Render)
+# Création des tables dès le démarrage
 with app.app_context():
     db.create_all()
 
